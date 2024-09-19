1b213ee2936ba194883833c07a840575e7566efa
Clint Airé <111376518+clintaire@users.noreply.github.com>
2024-09-19 20:18:17 +0100

    Remove all files to reset repository

--- PyGC/sample_code/sample_code.py
+++
@@ -1,8 +0,0 @@
-def calculate():
-    a = 5
-    b = a + 10
-    c = a = 15  # This should raise an issue (using = instead of ==)
-    d = b - 5
-
-def compare_values(x, y):
-    return x = y  # Incorrect, should be '=='
